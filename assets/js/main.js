cf139cb219e4ce7702e464a213fabdb92bd56116
Ezequiel Mamone <ezequielmamone@gmail.com>
2023-07-20 21:29:34 -0300

    Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,33 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+/* ===== MENU SHOW ===== */
+// Validate if constant exist
+if(navToggle){
+	navToggle.addEventListener("click", ()=>{
+		navMenu.classList.add('show-menu')
+	})
+}
+
+// Validate if constant exist
+if(navClose){
+	navClose.addEventListener("click", ()=>{
+		navMenu.classList.remove('show-menu')
+	})
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
 
+const linkAction = ()=>{
+	// When we click on each nav__link, we remove the show menu class
+	navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
